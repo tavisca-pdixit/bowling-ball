22b382abc1f6f95a0f3979b675246f6bc3837ab0
pdixit <pdixit@tavisca.com>
2014-07-15 17:53:06 +0530

    Possible test cases are considered and are working

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,14 +8,65 @@ namespace Bowling
 {
     public class Game
     {
+        public int final_score = 0,i=0;
+        public int[] pin = new int[30];
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            if ((pins == 10)&&((i % 2) == 0))
+            
+            {
+                pin[i] = pins;
+                i++;
+                pin[i] = 0;
+                i++;
+  
+            }
+            else
+            {
+                pin[i] = pins;
+                i++;
+            }
+
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            for (int j = 0; j < 20; j++)
+            {
+                if (pin[j] == 10)
+                {
+                    if ((j % 2) == 0)
+                    {
+                        if (((pin[j + 2]) == 10))
+                        {
+                            final_score += pin[j]+pin[j+2]+pin[j+4];
+                        }
+                        else
+                        {
+                            final_score = final_score + pin[j] + pin[j + 2] + pin[j + 3];
+                        }
+                        
+                    }
+                    else
+                    {
+                        final_score += pin[j] + pin[j + 1];
+                    }
+                }
+                else if ((j % 2) == 0)
+                {
+                    if ((pin[j] + pin[j + 1]) == 10)
+                    {
+                        final_score += pin[j] + pin[j + 1] + pin[j + 2];
+                        j++;
+                    }
+                    else
+                        final_score += pin[j];
+                }
+                else
+                    final_score += pin[j];
+            }
+            return final_score;
         }
 
     }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,14 +1,139 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bowling;
 
 namespace BowlingFixtures
 {
     [TestClass]
     public class GameFixture
     {
+        
         [TestMethod]
-        public void TestMethod1()
+        public void AllStrikes()
         {
+            Game obj = new Game();
+            int result = 0;
+
+            for (int i = 0; i < 12; i++)
+                obj.Roll(10);
+
+            result = obj.GetScore();
+            Assert.AreEqual(result, 300);
         }
+
+        [TestMethod]
+        public void AllZero()
+        {
+            Game obj = new Game();
+            int result = 0;
+
+            for (int i = 0; i < 12; i++)
+                obj.Roll(0);
+
+            result = obj.GetScore();
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void NoStrikeNoSpare()
+        {
+            Game obj = new Game();
+            int result = 0;
+            
+            obj.Roll(0);
+            obj.Roll(1);
+            obj.Roll(2);
+            obj.Roll(3);
+            obj.Roll(4);
+            obj.Roll(5);
+            obj.Roll(0);
+            obj.Roll(1);
+            obj.Roll(2);
+            obj.Roll(3);
+            obj.Roll(4);
+            obj.Roll(5);
+            obj.Roll(0);
+            obj.Roll(1);
+            obj.Roll(2);
+            obj.Roll(3);
+            obj.Roll(4);
+            obj.Roll(5);
+            obj.Roll(0);
+            obj.Roll(1);
+
+            result = obj.GetScore();
+            Assert.AreEqual(result, 16);
+        }
+
+         [TestMethod]
+          public void StrikeAtEnd()
+          {
+              Game obj = new Game();
+              int result = 0;
+
+              obj.Roll(4);
+              obj.Roll(1);
+              obj.Roll(2);
+              obj.Roll(3);
+              obj.Roll(4);
+              obj.Roll(5);
+              obj.Roll(1);
+              obj.Roll(2);
+              obj.Roll(3);
+              obj.Roll(4);
+              obj.Roll(5);
+              obj.Roll(0);
+              obj.Roll(1);
+              obj.Roll(2);
+              obj.Roll(3);
+              obj.Roll(4);
+              obj.Roll(1);
+              obj.Roll(3);
+              obj.Roll(10);
+              obj.Roll(5);
+              obj.Roll(4);
+
+              result = obj.GetScore();
+              Assert.AreEqual(result, 67);
+          }
+
+
+         [TestMethod]
+         public void Random1()
+         {
+             Game obj = new Game();
+             int result = 0;
+
+             obj.Roll(0);
+             obj.Roll(7);
+             obj.Roll(9);
+             obj.Roll(1);
+             obj.Roll(0);
+             obj.Roll(6);
+             obj.Roll(0);
+             obj.Roll(0);
+             obj.Roll(0);
+             obj.Roll(10);
+
+             result = obj.GetScore();
+             Assert.AreEqual(result, 33);
+         }
+
+         [TestMethod]
+         public void Random2()
+         {
+             Game obj = new Game();
+             int result = 0;
+
+             obj.Roll(0);
+             obj.Roll(1);
+             obj.Roll(2);
+             obj.Roll(3);
+             obj.Roll(4);
+             obj.Roll(6);
+
+             result = obj.GetScore();
+             Assert.AreEqual(result, 16);
+         }
     }
 }
